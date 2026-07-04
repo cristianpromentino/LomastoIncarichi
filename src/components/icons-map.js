6260c4e99b6a18a70d6294d4a86c2500b1e8e1c1
cristianpromentino <cristianpromentino@gmail.com>
2026-07-04 16:38:59 +0200

    Create icons-map.js
    Aggiunta mappa icone Solar

---
+++ src/components/icons-map.js
@@ -0,0 +1,28 @@
+// icons-map.js
+// Mappatura semantica NodoSuite -> icone Solar (stile LineDuotone).
+import {
+  House,
+  Buildings2,
+  DocumentAdd,
+  User,
+  Settings,
+  ChecklistMinimalistic,
+} from '@solar-icons/react-perf/LineDuotone';
+
+export const NAV_ICONS = {
+  home: House,
+  condomini: Buildings2,
+  verbali: DocumentAdd,
+  persone: User,
+  impostazioni: Settings,
+  incarichi: ChecklistMinimalistic,
+};
+
+export const NAV_LABELS = {
+  home: 'Home',
+  condomini: 'Condomini',
+  verbali: 'Verbali',
+  persone: 'Persone',
+  impostazioni: 'Impostazioni',
+  incarichi: 'Incarichi',
+};
